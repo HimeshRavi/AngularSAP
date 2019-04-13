85a9afb161d91568d60848ed69b0bc607ecfde56
Himesh Ravi <ravi.himesh@gmail.com>
2019-04-13 22:51:39 +1000

    added survey component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SurveyComponent } from './survey/survey.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'survey', component: SurveyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
